dd3b9bd08a3b4e5030485c05f52ff743778f81d3
tyler <tylah@riseup.net>
2023-05-07 15:34:36 +0100

    Initial commit

---
+++ src/main/java/com/store/onlinestoredemo/OnlineStoreDemoApplication.java
@@ -0,0 +1,13 @@
+package com.store.onlinestoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/store/onlinestoredemo/OnlineStoreDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.onlinestoredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
